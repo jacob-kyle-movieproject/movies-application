e7b6acc52cf4e0b816390fa2f9194c77ff397522
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-07-22 13:41:09 -0500

    get,post,put requests finished

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+const $ = require("jquery");
 module.exports = {
     getMovies: () => {
     return fetch('/api/movies')
@@ -6,5 +7,56 @@ module.exports = {
     getMovieInfo: (title) => {
     return fetch(`http://www.omdbapi.com/?s=${title}&type=movie&apikey=e66939d3`)
         .then(response => response.json())
+    },
+    addMovie: (arr) =>{
+        let origin = $("#post-title").val();
+        let rate = $('#post-rate').val();
+        let title;
+        let poster;
+            fetch( `http://www.omdbapi.com/?s=${origin}&r=json&type=movie&apikey=e66939d3`)
+            .then(response => response.json())
+            .then(movies =>{
+                console.log(movies);
+                poster = movies.Search[0].Poster;
+                title = movies.Search[0].Title;
+
+        const url = '/api/movies';
+        const movie = {
+            "title": title,
+            "rating": rate,
+            "id": (arr.length),
+            "poster": poster
+        };
+        // console.log(url);
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        };
+        fetch(url, options)
+            })
+    },
+    updateMovie:  (arr,counter)=>{
+        // let origin = $("#get-title").val();
+        let rate = $('#get-rate').val();
+        console.log(rate);
+        console.log(arr);
+        const url = `/api/movies/${counter}`;
+        const movie = {
+            "title": arr[counter].title,
+            "rating": rate,
+            "id": arr[counter].id,
+            "poster": arr[counter].poster
+        };
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movie),
+        };
+        fetch(url, options)
     }
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@
 /**
  * require style imports
  */
+let count = 0;
 const $ = require("jquery");
-const {getMovies,getMovieInfo} = require('./api.js');
+const {getMovies,getMovieInfo,addMovie,updateMovie} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
@@ -19,13 +20,28 @@ getMovies().then((movies) => {
   console.log(error);
 });
 $("#search").on("click",function(){
-  let movieTitle = $("#title");
+  let movieTitle = $("#get-title");
   getMovies().then((data)=>{
-      data.forEach(({title, rating, id}) => {
+      data.forEach(({title, rating,poster, id}) => {
         if(title.indexOf(movieTitle.val()) > -1) {
         movieTitle.val(title);
+        $("#get-rate").val(rating);
+        $(".picture").html(`<img src=${poster} alt="poster">`);
         console.log(title);
+          console.log(id);
+          count = id;
         }
       });
   });
+});
+$("#update").on("click",function(){
+  getMovies().then(data => {
+    updateMovie(data,count)
+  })
+});
+$("#add").on("click",function(){
+  let movieArr;
+  getMovies().then(data =>{
+    addMovie(data)
+  })
 });
