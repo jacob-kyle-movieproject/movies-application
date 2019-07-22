995ac98fc792a2d8c18be637b26dac69dd2f94c1
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-07-22 11:26:30 -0500

    morestuff

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 module.exports = {
-  getMovies: () => {
+    getMovies: () => {
     return fetch('/api/movies')
-      .then(response => response.json());
-  }
+      .then(response => response.json())
+    },
+    getMovieInfo: (title) => {
+    return fetch(`http://www.omdbapi.com/?s=${title}&type=movie&apikey=e66939d3`)
+        .then(response => response.json())
+    }
 };
